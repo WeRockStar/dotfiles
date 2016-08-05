8052f908476fb29bc909223f90a7e9daf7b816db
WeRockStar <vr_freestyle@outlook.com>
2016-08-05 20:56:39 +0700

    Update android commands

--- android/aliases.zsh
+++ android/aliases.zsh
@@ -1,8 +1,9 @@
 export JAVA_HOME=$(/usr/libexec/java_home)
-export GROOVY_HOME=/usr/local/opt/groovy/libexec
 export PATH=$PATH:$JAVA_HOME/bin
 export ANDROID_HOME=/Users/WeRockStar/Desktop/android-sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-alias adbd="adb devices"
+alias ad="adb devices"
+alias arb="adb reboot"
+alias are="adb reconnect"
