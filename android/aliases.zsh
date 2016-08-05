8d42e0b428a7172a2c797ab7c2fda5a7aeb12d76
WeRockStar <vr_freestyle@outlook.com>
2016-08-05 20:51:44 +0700

    Add new android command

---
+++ android/aliases.zsh
@@ -0,0 +1,8 @@
+export JAVA_HOME=$(/usr/libexec/java_home)
+export GROOVY_HOME=/usr/local/opt/groovy/libexec
+export PATH=$PATH:$JAVA_HOME/bin
+export ANDROID_HOME=/Users/WeRockStar/Desktop/android-sdk
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+alias adbd="adb devices"
