64563a640299a2de0debfbca5ada785a2b8f47b2
WeRockStar <vr_freestyle@outlook.com>
2016-08-05 20:40:22 +0700

    Add git stash pop

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,3 +20,4 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias glr="git pull --rebase"
 alias gst="git stash"
+alias gstp="git stash pop"
