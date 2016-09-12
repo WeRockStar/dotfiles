3fcf362f8387dde82932b232dcf5fccc327473dd
WeRockStar <vr_freestyle@outlook.com>
2016-09-12 21:16:30 +0700

    Update android command

--- android/aliases.zsh
+++ android/aliases.zsh
@@ -3,6 +3,12 @@ if (( $+commands[adb] ))
  then
    alias adb=$android_path
  fi
+ export ANDROID_HOME=/Users/WeRockStar/Desktop/android-sdk
+ export PATH=$PATH:$ANDROID_HOME/tools
+ export PATH=$PATH:$ANDROID_HOME/platform-tools
 
  alias ad="adb devices"
  alias arb="adb reboot"
+ alias alg="adb logcat"
+ alias aks="adb kill-server"
+ alias ass="adb start-server"
